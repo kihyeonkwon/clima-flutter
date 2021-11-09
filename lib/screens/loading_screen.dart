3048f5b461c0fd1e8450630710bcf1d8796677bb
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2021-11-09 23:20:57 +0900

    Add weather update

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+const appid = '4af4d07cb88e48380fc2418e6f5646d7';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,6 +11,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double lat;
+  double lon;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -17,8 +24,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getLocation();
-    print(location.longitude);
-    print(location.latitude);
+    lat = location.latitude;
+    lon = location.longitude;
+  }
+
+  void getWeather() async {
+    var url = Uri.https('api.openweathermap.org', '/data/2.5/weather',
+        {'lat': '$lat', 'lon': '$lon', 'appid': '$appid'});
+
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      print(jsonResponse);
+      double temp = jsonResponse["main"]["temp"];
+      int condition = jsonResponse["weather"][0]["id"];
+      String city = jsonResponse["name"];
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -26,7 +50,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {},
+          onPressed: () {
+            getWeather();
+          },
           child: Text('Get Location'),
         ),
       ),
